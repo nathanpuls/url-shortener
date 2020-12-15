91968b3b39bb4bb3c92037e7a9352f56d807ea3e
Nathan Puls <71989998+nathanpuls@users.noreply.github.com>
2020-12-15 16:39:27 -0600

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const ShortUrl = require('./models/shortUrl')
 const app = express()
 
-mongoose.connect('mongodb://localhost/urlShortener', {
+mongoose.connect('mongodb+srv://nathan1234:nathan1234@cluster0.xn6cp.mongodb.net/test?retryWrites=true&w=majority', {
   useNewUrlParser: true, useUnifiedTopology: true
 })
 
